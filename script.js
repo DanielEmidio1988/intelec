aa5aa87bb78e6c55b8af158aae8570d0560a6a6b
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2023-07-28 11:36:18 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const headersAccordion = document.querySelectorAll('.accordion-header')
+const accordionItens = document.querySelectorAll('.accordion-item')
+
+headersAccordion.forEach(function(header){
+    
+    header.addEventListener('click',function(){
+        const item = this.parentNode
+    
+        const isActive = item.classList.contains('accordion-item--active')
+
+        accordionItens.forEach(function(item){
+            item.classList.remove('accordion-item--active')
+            item.classList.add('accordion-item--closed')
+        })
+
+        if(!isActive){
+            item.classList.add('accordion-item--active')
+            item.classList.remove('accordion-item--closed')
+        }
+
+    })
+
+})
